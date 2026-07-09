1022c89e6746ecd0b8c9587ec3fc817c522e89f5
nVuln <j@sax.vn>
2026-07-09 14:17:46 +0700

    Add new Lazada API methods: GetMultipleOrderItems, GetOVOOrders, OrderCancelValidate, and SetInvoiceNumber

--- src/Resources/Order.php
+++ src/Resources/Order.php
@@ -63,6 +63,56 @@ class Order extends Resource
     }
 
     /**
+     * @see https://open.lazada.com/apps/doc/api?path=%2Forders%2Fitems%2Fget
+     */
+    public function GetMultipleOrderItems($order_ids)
+    {
+        return $this->call('GET', 'orders/items/get', [
+            RequestOptions::QUERY => [
+                'order_ids' => static::formatListIds($order_ids),
+            ]
+        ]);
+    }
+
+    /**
+     * @see https://open.lazada.com/apps/doc/api?path=%2Forders%2Fovo%2Fget
+     */
+    public function GetOVOOrders($tradeOrderIds)
+    {
+        return $this->call('GET', 'orders/ovo/get', [
+            RequestOptions::QUERY => [
+                'tradeOrderIds' => $tradeOrderIds,
+            ]
+        ]);
+    }
+
+    /**
+     * @see https://open.lazada.com/apps/doc/api?path=%2Forder%2Freverse%2Fcancel%2Fvalidate
+     */
+    public function OrderCancelValidate($order_id, $order_item_id_list)
+    {
+        return $this->call('GET', 'order/reverse/cancel/validate', [
+            RequestOptions::QUERY => [
+                'order_id' => $order_id,
+                'order_item_id_list' => static::formatListIds($order_item_id_list),
+            ]
+        ]);
+    }
+
+    /**
+     * @see https://open.lazada.com/apps/doc/api?path=%2Forder%2Finvoice_number%2Fset
+     */
+    public function SetInvoiceNumber($order_item_id, $invoice_number)
+    {
+        return $this->call('POST', 'order/invoice_number/set', [
+            RequestOptions::FORM_PARAMS => [
+                'order_item_id' => $order_item_id,
+                'invoice_number' => $invoice_number,
+            ]
+        ]);
+    }
+
+    /**
      * @see https://open.lazada.com/apps/doc/api?path=%2Forder%2Frts
      */
     public function SetStatusToReadyToShip($item_ids, $shipment_provider, $tracking_number)
